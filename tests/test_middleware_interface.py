192e225a188d49906268027dee62ed19427b5905
John Parejko <parejkoj@uw.edu>
2022-03-09 17:37:20 -0800

    Improve tests and start using butler API calls
    The file included here is empty: it just needs to exist, we mock its metadata.
    
    Update table file.

--- tests/test_middleware_interface.py
+++ tests/test_middleware_interface.py
@@ -19,20 +19,67 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import os.path
 import tempfile
+import os.path
 import unittest
+import unittest.mock
+
+import astropy.units as u
+
+import astro_metadata_translator
+from lsst.daf.butler import Butler, DataCoordinate
+from lsst.obs.base.formatters.fitsExposure import FitsImageFormatter
+from lsst.obs.base.ingest import RawFileDatasetInfo, RawFileData
+import lsst.resources
 
 from activator.middleware_interface import MiddlewareInterface
 from activator.visit import Visit
 
 
+def fake_file_data(filename, dimensions, instrument):
+    """Return file data for a mock file to be ingested.
+
+    Parameters
+    ----------
+    filename : `str`
+        Full path to the file to mock. Can be a non-existant file.
+    dimensions : `lsst.daf.butler.DimensionsUniverse`
+        The full set of dimensions for this butler.
+    instrument : `lsst.obs.base.Instrument`
+        The instrument the file is supposed to be from.
+
+    Returns
+    -------
+    data_id, file_data, : `DataCoordinate`, `RawFileData`
+        The id and descriptor for the mock file.
+    """
+    data_id = DataCoordinate.standardize({"exposure": 1, "detector": 1, "instrument": instrument.getName()},
+                                         universe=dimensions)
+    obs_info = astro_metadata_translator.makeObservationInfo(instrument=instrument.getName(),
+                                                             exposure_id=1,
+                                                             observation_id="1",
+                                                             physical_filter="r",
+                                                             exposure_time=30.0*u.second)
+    dataset_info = RawFileDatasetInfo(data_id, obs_info)
+    file_data = RawFileData([dataset_info],
+                            lsst.resources.ResourcePath(filename),
+                            FitsImageFormatter,
+                            instrument)
+    return data_id, file_data
+
+
 class MiddlewareInterfaceTest(unittest.TestCase):
+    """Test the MiddlewareInterface class with faked data.
+    """
     def setUp(self):
-        input_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data")
-        instrument = "TestCam"
-        self.output_dir = tempfile.TemporaryDirectory()
-        self.interface = MiddlewareInterface(input_dir, self.output_dir, instrument)
+        data_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data")
+        input_repo = os.path.join(data_dir, "input_repo")
+        instrument = "lsst.obs.lsst.LsstCam"
+        self.input_data = os.path.join(data_dir, "input_data")
+        self.repo = tempfile.TemporaryDirectory()
+        butler = Butler(Butler.makeRepo(self.repo.name), writeable=True)
+        self.interface = MiddlewareInterface(input_repo, self.input_data, instrument, butler,
+                                             prefix="file://")
         self.next_visit = Visit(instrument,
                                 detector=1,
                                 group=1,
@@ -44,26 +91,68 @@ class MiddlewareInterfaceTest(unittest.TestCase):
         self.logger_name = "lsst.activator.middleware_interface"
 
     def test_init(self):
-        """Basic tests of initializing an interface object.
+        """Basic tests of the initialized interface object.
         """
-        # did we get a useable butler instance?
-        self.assertIn("/tmp/butler-", self.interface.dest.datastore.root.path)
         # Ideas for things to test:
         # * On init, does the right kind of butler get created, with the right
         #   collections, etc?
         # * On init, is the local butler repo purely in memory?
 
+        # Check that the butler instance is properly configured.
+        instruments = list(self.interface.butler.registry.queryDimensionRecords("instrument"))
+        self.assertEqual("LSSTCam", instruments[0].name)
+
+        # Check that the ingester is properly configured.
+        self.assertEqual(self.interface.rawIngestTask.config.failFast, True)
+        self.assertEqual(self.interface.rawIngestTask.config.transfer, "copy")
+
     def test_prep_butler(self):
+        """Test that the butler has all necessary data for the next visit.
+        """
         with self.assertLogs(self.logger_name, level="INFO") as cm:
             self.interface.prep_butler(self.next_visit)
         msg = f"INFO:{self.logger_name}:Preparing Butler for visit '{self.next_visit}'"
         self.assertEqual(cm.output, [msg])
+        # TODO: Test that we have appropriate refcats?
 
     def test_ingest_image(self):
-        with self.assertLogs(self.logger_name, level="INFO") as cm:
-            self.interface.ingest_image(self.next_visit)
-        msg = f"INFO:{self.logger_name}:Ingesting image id '{self.next_visit}'"
-        self.assertEqual(cm.output, [msg])
+        filename = "fakeRawImage.fits"
+        filepath = os.path.join(self.input_data, filename)
+        data_id, file_data = fake_file_data(filepath,
+                                            self.interface.butler.dimensions,
+                                            self.interface.instrument)
+        with unittest.mock.patch.object(self.interface.rawIngestTask, "extractMetadata") as mock:
+            mock.return_value = file_data
+            self.interface.ingest_image(filename)
+
+            datasets = list(self.interface.butler.registry.queryDatasets('raw',
+                                                                         collections=['LSSTCam/raw/all']))
+            self.assertEqual(datasets[0].dataId, data_id)
+
+    def test_ingest_image_fails_missing_file(self):
+        """Trying to ingest a non-existent file should raise.
+
+        NOTE: this is currently a bit of a placeholder: I suspect we'll want to
+        change how errors are handled in the interface layer, raising custom
+        exceptions so that the activator can deal with them better. So even
+        though all this is demonstrating is that if the file doesn't exist,
+        rawIngestTask.run raises FileNotFoundError and that gets passed up
+        through ingest_image(), we'll want to have a test of "missing file
+        ingestion", and this can serve as a starting point.
+        """
+        filename = "nonexistentImage.fits"
+        filepath = os.path.join(self.input_data, filename)
+        data_id, file_data = fake_file_data(filepath,
+                                            self.interface.butler.dimensions,
+                                            self.interface.instrument)
+        with unittest.mock.patch.object(self.interface.rawIngestTask, "extractMetadata") as mock, \
+                self.assertRaisesRegex(FileNotFoundError, "Resource at .* does not exist"):
+            mock.return_value = file_data
+            self.interface.ingest_image(filename)
+        # There should not be any raw files in the registry.
+        datasets = list(self.interface.butler.registry.queryDatasets('raw',
+                                                                     collections=['LSSTCam/raw/all']))
+        self.assertEqual(datasets, [])
 
     def test_run_pipeline(self):
         with self.assertLogs(self.logger_name, level="INFO") as cm:
