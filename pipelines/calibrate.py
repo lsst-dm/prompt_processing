fbbb38af24243cab30d2fec97f0d81f863462649
Hsin-Fang Chiang <hchiang@lsst.org>
2023-08-25 12:27:34 -0700

    Refresh the unit test data repo with ap_verify_ci_hits2015
    The unit test central repo was made using ap_verify_ci_hits2015
    data repo. DM-38449 changed its skymap and templates.
    DM-38853 removed the renaming of the catalogs to gaia and panstarrs
    and insertion of the refcats into a specific run.
    Here we rebuild the unit test data repo with the current
    ap_verify_ci_hits2015 and update the parameters accordingly.

--- pipelines/calibrate.py
+++ pipelines/calibrate.py
@@ -5,9 +5,9 @@
 # Use gaia for astrometry (phot_g_mean for everything, as that is the broadest
 # band with the most depth).
 
-config.connections.astromRefCat = "gaia"
+config.connections.astromRefCat = "gaia_dr2_20200414"
 config.astromRefObjLoader.anyFilterMapsToThis = "phot_g_mean"
 config.astromRefObjLoader.filterMap = {}
 
 # Use panstarrs for photometry (grizy filters).
-config.connections.photoRefCat = "panstarrs"
+config.connections.photoRefCat = "ps1_pv3_3pi_20170110"
--- tests/data/make_central_repo.sh
+++ tests/data/make_central_repo.sh
@@ -4,19 +4,19 @@
 # match.
 
 # This export file was created with this command, and then added a comment on the first line:
-# make_preloaded_export.py --src-rep $AP_VERIFY_CI_HITS2015_DIR
+# make_preloaded_export.py --src-rep $AP_VERIFY_CI_HITS2015_DIR/preloaded
 butler create central_repo
 butler import --export-file export.yaml --transfer copy central_repo/ $AP_VERIFY_CI_HITS2015_DIR/preloaded/
 
-butler collection-chain central_repo refcats refcats/gen2
-butler collection-chain central_repo DECam/templates templates/deep
+butler collection-chain central_repo refcats refcats/DM-28636
+butler collection-chain central_repo DECam/templates u/elhoward/DM-38243/templates/20230320T214145Z
 butler collection-chain central_repo DECam/defaults DECam/calib,refcats,DECam/templates,skymaps
 
 # Empty out files and make them size 0 in the registry.
 # We do not empty the camera description in ``DECAM/calib/unbounded`
 # because we need it to load the camera geometry.
 find central_repo/refcats -name "*.fits" -execdir sh -c '> "$1"' -- {} \;
-find central_repo/templates -name "*.fits" -execdir sh -c '> "$1"' -- {} \;
+find central_repo -name "*templates*fits" -execdir sh -c '> "$1"' -- {} \;
 find central_repo/DECam/calib/20150218T000000Z -name "*.fits" -execdir sh -c '> "$1"' -- {} \;
 find central_repo/DECam/calib/20150313T000000Z -name "*.fits" -execdir sh -c '> "$1"' -- {} \;
 find central_repo/DECam/calib/curated -name "*.fits" -execdir sh -c '> "$1"' -- {} \;
@@ -24,4 +24,4 @@ find central_repo/DECam/calib/curated -name "*.fits" -execdir sh -c '> "$1"' -- 
 
 # The camera and skymap files are not emptied out. The records need to be consistent so that
 # the datastore does not complain about size mismatch when reading them.
-sqlite3 central_repo/gen3.sqlite3 'update file_datastore_records set file_size=0 where path != "DECam/calib/unbounded/camera/camera_DECam_DECam_calib_unbounded.fits" and path != "skymaps/skyMap/skyMap_deepCoadd_skyMap_skymaps.pickle";'
+sqlite3 central_repo/gen3.sqlite3 'update file_datastore_records set file_size=0 where path != "DECam/calib/unbounded/camera/camera_DECam_DECam_calib_unbounded.fits" and path != "skymaps/skyMap/skyMap_decam_rings_v1_skymaps.pickle";'
