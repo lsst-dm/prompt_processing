f7051b84a3c363964ce65cad107b63d889be683d
Krzysztof Findeisen <kfindeis@uw.edu>
2023-10-31 13:39:15 -0700

    Improve signal logging.

--- python/activator/activator.py
+++ python/activator/activator.py
@@ -240,7 +240,7 @@ def next_visit_handler() -> Tuple[str, int]:
     try:
         # Do nothing until timeout sequence starts
         sig = signal.sigwait({signal.SIGTERM, signal.SIGABRT})
-        _log.info(f"Signal {sig} detected. Shutting down...")
+        _log.info(f"Signal {signal.Signals(sig).name} detected. Shutting down...")
 
         # Simulate a yellow-light shutdown
         time.sleep(10.0)
