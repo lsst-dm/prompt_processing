5fc757b1f0754d8e85dbd77473a3fec4c62a85a8
Krzysztof Findeisen <kfindeis@uw.edu>
2024-08-09 11:32:35 -0700

    Move Gunicorn options to config file.
    Using a config file makes the command line in the Dockerfile much more
    concise, and avoids gratuitous changes to the file (and the build
    tests those require).
    
    I've kept --bind in the Dockerfile because the binding can be set at
    build time using an ARG.

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -1,6 +1,21 @@
+import os
 import shutil
 
 import activator.repo_tracker
+
+
+# Config options
+# --------------
+
+workers = 1
+
+threads = 1
+worker_class = "sync"
+
+# Normally defined in the Kubernetes config
+graceful_timeout = int(os.environ.get("WORKER_GRACE_PERIOD", 30))
+timeout = int(os.environ.get("WORKER_TIMEOUT", 0))
+max_requests = int(os.environ.get("WORKER_RESTART_FREQ", 0))
 
 
 # Hooks run on the master process
