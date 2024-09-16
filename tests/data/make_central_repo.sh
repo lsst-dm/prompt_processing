674a4b1ff63a590ea30b547bf048d34f53d57e18
Krzysztof Findeisen <kfindeis@uw.edu>
2024-09-16 11:52:03 -0700

    Use ComCamSim OR4 as unit test data.
    DECam is increasingly unrepresentative of expected LSST data, and the
    original dataset (adapted from ap_verify) had a nonstandard repository
    layout that had to be special-cased. ComCamSim should provide a more
    realistic data set going forward.
    
    The unit test repository remains at dimensions-config 6.

--- tests/data/make_central_repo.sh
+++ tests/data/make_central_repo.sh
@@ -3,25 +3,41 @@
 # tests, making all the files be empty and adjusting the sqlite registry to
 # match.
 
-# This export file was created with this command, and then added a comment on the first line:
-# make_preloaded_export.py --src-rep $AP_VERIFY_CI_HITS2015_DIR/preloaded
-butler create central_repo
-butler import --export-file export.yaml --transfer copy central_repo/ $AP_VERIFY_CI_HITS2015_DIR/preloaded/
+REPO="${PROMPT_PROCESSING_DIR}/tests/data/central_repo/:?Can't find prompt_processing repo; is it set up?"
 
-butler collection-chain central_repo refcats refcats/DM-28636
-butler collection-chain central_repo DECam/templates u/elhoward/DM-38243/templates/20230320T214145Z
-butler collection-chain central_repo DECam/defaults DECam/calib,refcats,DECam/templates,skymaps
+butler create "$REPO"
+butler register-instrument "$REPO" lsst.obs.lsst.LsstComCamSim
+
+# Import datasets
+butler write-curated-calibrations "$REPO" LSSTComCamSim --collection LSSTComCamSim/calib/DM-46312
+butler transfer-datasets embargo_or4 "$REPO" --transfer copy --register-dataset-types --transfer-dimensions --dataset-type bfk --collections u/abrought/bfk_70240217_w_2024_07_final/20240227T033735Z --where "instrument='LSSTComCamSim' and detector in (4, 5)"
+butler transfer-datasets embargo_or4 "$REPO" --transfer copy --register-dataset-types --transfer-dimensions --dataset-type bias --collections LSSTComCamSim/calib/DM-42287 --where "instrument='LSSTComCamSim' and detector in (4, 5)"
+butler transfer-datasets embargo_or4 "$REPO" --transfer copy --register-dataset-types --transfer-dimensions --dataset-type dark --collections LSSTComCamSim/calib/DM-42287 --where "instrument='LSSTComCamSim' and detector in (4, 5)"
+butler transfer-datasets embargo_or4 "$REPO" --transfer copy --register-dataset-types --transfer-dimensions --dataset-type flat --collections LSSTComCamSim/calib/DM-44910 --where "instrument='LSSTComCamSim' and detector in (4, 5) and physical_filter='g_01'"
+butler transfer-datasets /repo/main "$REPO" --transfer copy --register-dataset-types --transfer-dimensions --dataset-type pretrainedModelPackage --collections pretrained_models/dummy
+butler transfer-datasets embargo_or4 "$REPO" --transfer copy --register-dataset-types --transfer-dimensions --dataset-type skyMap --where "skymap='ops_rehersal_prep_2k_v1'"
+butler transfer-datasets embargo_or4 "$REPO" --transfer copy --register-dataset-types --transfer-dimensions --dataset-type goodSeeingCoadd --collections LSSTComCamSim/runs/OR4_templates/w_2024_23/DM-44718/20240610T044930Z --where "instrument='LSSTComCamSim' and detector=4 and visit=7024061700046"
+butler transfer-datasets embargo_or4 "$REPO" --transfer copy --register-dataset-types --transfer-dimensions --dataset-type uw_stars_20240524 --collections refcats --where "instrument='LSSTComCamSim' and detector=4 and visit=7024061700046"
+
+# Certify non-curated calibs
+butler certify-calibrations "$REPO" u/abrought/bfk_70240217_w_2024_07_final/20240227T033735Z LSSTComCamSim/calib/DM-46312 bfk --begin-date "2024-06-15T12:00:00" --end-date "2024-06-25T12:00:00"
+butler certify-calibrations "$REPO" u/jchiang/bias_70240217_w_2024_07/20240218T190659Z LSSTComCamSim/calib/DM-46312 bias --begin-date "2024-06-15T12:00:00" --end-date "2024-06-25T12:00:00"
+butler certify-calibrations "$REPO" u/jchiang/dark_70240217_w_2024_07/20240218T191310Z LSSTComCamSim/calib/DM-46312 dark --begin-date "2024-06-15T12:00:00" --end-date "2024-06-25T12:00:00"
+butler certify-calibrations "$REPO" u/jchiang/flat_70240417_w_2024_15/20240418T050546Z LSSTComCamSim/calib/DM-46312 flat --begin-date "2024-06-15T12:00:00" --end-date "2024-06-25T12:00:00"
+
+# Alias all the runs
+butler collection-chain "$REPO" pretrained_models pretrained_models/dummy
+butler collection-chain "$REPO" refcats refcats/DM-42510
+butler collection-chain "$REPO" LSSTComCamSim/calib LSSTComCamSim/calib/DM-46312
+butler collection-chain "$REPO" LSSTComCamSim/templates LSSTComCamSim/runs/OR4_templates/w_2024_23/DM-44718/20240610T044930Z
+butler collection-chain "$REPO" LSSTComCamSim/defaults LSSTComCamSim/calib skymaps refcats pretrained_models
 
 # Empty out files and make them size 0 in the registry.
-# We do not empty the camera description in ``DECAM/calib/unbounded`
-# because we need it to load the camera geometry.
-find central_repo/refcats -name "*.fits" -execdir sh -c '> "$1"' -- {} \;
-find central_repo -name "*templates*fits" -execdir sh -c '> "$1"' -- {} \;
-find central_repo/DECam/calib/20150218T000000Z -name "*.fits" -execdir sh -c '> "$1"' -- {} \;
-find central_repo/DECam/calib/20150313T000000Z -name "*.fits" -execdir sh -c '> "$1"' -- {} \;
-find central_repo/DECam/calib/curated -name "*.fits" -execdir sh -c '> "$1"' -- {} \;
-# find central_repo/skymaps -name "*.fits" -execdir sh -c '> "$1"' -- {} \;
-
-# The camera and skymap files are not emptied out. The records need to be consistent so that
-# the datastore does not complain about size mismatch when reading them.
-sqlite3 central_repo/gen3.sqlite3 'update file_datastore_records set file_size=0 where path != "DECam/calib/unbounded/camera/camera_DECam_DECam_calib_unbounded.fits" and path != "skymaps/skyMap/skyMap_decam_rings_v1_skymaps.pickle";'
+# We do not empty the camera or skymap because we actually need to read them.
+for x in `find "$REPO/LSSTComCamSim/calib/curated/" -name "*.fits"`; do : > $x; done
+for x in `find "$REPO/LSSTComCamSim/calib/DM-43441/" -name "*.fits"`; do : > $x; done
+for x in `find "$REPO/LSSTComCamSim/runs/" -name "*.fits"`; do : > $x; done
+for x in `find "$REPO/pretrained_models/" -name "*.zip"`; do : > $x; done
+for x in `find "$REPO/refcats/DM-42510/" -name "*.fits"`; do : > $x; done
+for x in `find "$REPO/u/abrought/" -name "*.fits"`; do : > $x; done
+for x in `find "$REPO/u/jchiang/" -name "*.fits"`; do : > $x; done
