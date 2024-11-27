e7450666dfd1661177472ac57490241f4d410e4b
Krzysztof Findeisen <kfindeis@uw.edu>
2024-11-26 16:22:37 -0800

    Support ra/dec ranges in PipelinesConfig.
    While ra/dec ranges can be used to directly constrain pipelines to a
    particular region, almost all the infrastructure needed to support
    ranges is also needed to support other position queries.

--- tests/test_config.py
+++ tests/test_config.py
@@ -193,3 +193,121 @@ class PipelinesConfigTest(unittest.TestCase):
             config.get_pipeline_files(dataclasses.replace(self.visit, survey="")),
             [os.path.normpath(os.path.join(getPackageDir("ap_pipe"), "pipelines", "Isr.yaml"))]
         )
+
+    def _check_boresight(self, config, inside_visit, outside_visits):
+        self.assertEqual(config.get_pipeline_files(inside_visit), [os.path.abspath("ApPipe.yaml")])
+        for v in outside_visits:
+            self.assertEqual(config.get_pipeline_files(v), [])
+
+        # Rotation is ignored for boresight constraints
+        for rot in FannedOutVisit.RotSys:
+            self.assertEqual(
+                config.get_pipeline_files(dataclasses.replace(inside_visit, rotationSystem=rot)),
+                [os.path.abspath("ApPipe.yaml")]
+            )
+        # If no coordinates, all ra constraints automatically fail
+        self.assertEqual(
+            config.get_pipeline_files(
+                dataclasses.replace(inside_visit, coordinateSystem=FannedOutVisit.CoordSys.NONE)),
+            []
+        )
+        with self.assertRaises(ValueError):
+            config.get_pipeline_files(
+                dataclasses.replace(inside_visit, coordinateSystem=FannedOutVisit.CoordSys.OBSERVED))
+        with self.assertRaises(ValueError):
+            config.get_pipeline_files(
+                dataclasses.replace(inside_visit, coordinateSystem=FannedOutVisit.CoordSys.MOUNT))
+
+    def test_ra_boresight(self):
+        config = PipelinesConfig([{"survey": "TestSurvey",
+                                   "pipelines": ["ApPipe.yaml"],
+                                   "ra": {"min": 100.0, "max": 150.0},
+                                   },
+                                  {"pipelines": None},
+                                  ])
+        self._check_boresight(config,
+                              self.visit,
+                              [dataclasses.replace(self.visit, position=[99.9, -65.3261])],
+                              )
+
+    def test_ra_wrap(self):
+        for config in [PipelinesConfig([{"survey": "TestSurvey",
+                                         "pipelines": ["ApPipe.yaml"],
+                                         "ra": {"min": 350.0, "max": 10.0},
+                                         },
+                                        {"pipelines": None},
+                                        ]),
+                       PipelinesConfig([{"survey": "TestSurvey",
+                                         "pipelines": ["ApPipe.yaml"],
+                                         "ra": {"min": -10.0, "max": 10.0},
+                                         },
+                                        {"pipelines": None},
+                                        ]),
+                       ]:
+            self._check_boresight(config,
+                                  dataclasses.replace(self.visit, position=[-5.0, 35.5]),
+                                  [dataclasses.replace(self.visit, position=[10.1, 35.5]),
+                                   dataclasses.replace(self.visit, position=[-10.1, 35.5]),
+                                   dataclasses.replace(self.visit, position=[349.9, 35.5]),
+                                   ]
+                                  )
+
+    def test_dec_boresight(self):
+        config = PipelinesConfig([{"survey": "TestSurvey",
+                                   "pipelines": ["ApPipe.yaml"],
+                                   "dec": {"min": -100.0, "max": -20.0},
+                                   },
+                                  {"pipelines": None},
+                                  ])
+        self._check_boresight(config,
+                              self.visit,
+                              [dataclasses.replace(self.visit, position=[134.5454, -19.99])],
+                              )
+
+    def test_radec_boresight(self):
+        config = PipelinesConfig([{"survey": "TestSurvey",
+                                   "pipelines": ["ApPipe.yaml"],
+                                   "ra": {"min": 100.0, "max": 150.0},
+                                   "dec": {"min": -100.0, "max": -20.0},
+                                   },
+                                  {"pipelines": None},
+                                  ])
+        self._check_boresight(config,
+                              self.visit,
+                              [dataclasses.replace(self.visit, position=[150.1, -65.3261]),
+                               dataclasses.replace(self.visit, position=[134.5454, -19.99]),
+                               dataclasses.replace(self.visit, position=[99.9, -19.99]),
+                               ])
+
+    def test_radec_zero(self):
+        config = PipelinesConfig([{"survey": "TestSurvey",
+                                   "pipelines": ["ApPipe.yaml"],
+                                   "ra": {"min": 0.0, "max": 150.0},
+                                   "dec": {"min": -100.0, "max": 0.0},
+                                   },
+                                  {"pipelines": None},
+                                  ])
+        self._check_boresight(config,
+                              dataclasses.replace(self.visit, position=[0.0, 0.0]),
+                              [dataclasses.replace(self.visit, position=[359.9, -25.0]),
+                               dataclasses.replace(self.visit, position=[10.0, 0.1]),
+                               ])
+
+        config = PipelinesConfig([{"survey": "TestSurvey",
+                                   "pipelines": ["ApPipe.yaml"],
+                                   "ra": {"min": 270.0, "max": 0.0},
+                                   "dec": {"min": -100.0, "max": 0.0},
+                                   },
+                                  {"pipelines": None},
+                                  ])
+        self._check_boresight(config,
+                              dataclasses.replace(self.visit, position=[0.0, 0.0]),
+                              [dataclasses.replace(self.visit, position=[0.1, -25.0]),
+                               dataclasses.replace(self.visit, position=[-10.0, 0.1]),
+                               ])
+
+    def test_dec_invalid(self):
+        with self.assertRaises(ValueError):
+            PipelinesConfig([{"survey": "TestSurvey", "pipelines": [], "dec": {"max": -20.0}}])
+        with self.assertRaises(ValueError):
+            PipelinesConfig([{"survey": "TestSurvey", "pipelines": [], "dec": {"min": 80.0}}])
