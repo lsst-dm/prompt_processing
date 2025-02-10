ed3f5c1186a30fd34fbf22a11e136e000cc27e5b
Krzysztof Findeisen <kfindeis@uw.edu>
2025-02-10 12:13:36 -0800

    Fix line-break bug in upload.py message.

--- python/tester/upload.py
+++ python/tester/upload.py
@@ -348,7 +348,7 @@ def upload_from_raws(kafka_url, instrument, raw_pool, src_bucket, dest_bucket, n
     """
     if n_groups > len(raw_pool):
         raise ValueError(f"Requested {n_groups} groups, but only {len(raw_pool)} "
-                         "unobserved raw groups are available."
+                         "unobserved raw groups are available. "
                          "For large-scale tests, consider upload_from_repo.py.")
 
     for i, true_group in enumerate(itertools.islice(raw_pool, n_groups)):
