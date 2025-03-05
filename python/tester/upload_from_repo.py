5ee1b0dca5262469d3a147540adedf80cbc8f79c
Kian-Tat Lim <ktl@slac.stanford.edu>
2025-03-05 18:28:15 +0000

    Data passed to put_object needs to be bytes.

--- python/tester/upload_from_repo.py
+++ python/tester/upload_from_repo.py
@@ -360,7 +360,7 @@ def _upload_one_image(temp_dir, group_id, butler, ref):
                             Body=json.dumps(dict(hdul[0].header)),
                             Key=dest_key.removesuffix("fits") + "json",
                         )
-            with open(path, "r") as temp_file:
+            with open(path, "rb") as temp_file:
                 dest_bucket.put_object(Body=temp_file, Key=dest_key)
             _log.debug(f"{dest_key} was written at {dest_bucket}")
         finally:
