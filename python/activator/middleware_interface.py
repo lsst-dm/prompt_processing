8eef418bbb87bb1d34372d67e01d355dedeecc41
Krzysztof Findeisen <kfindeis@uw.edu>
2025-03-06 14:24:07 -0800

    Refactor _find_data_to_preload.
    The old organization made it easy to accidentally miss a category of
    data, as happened with ML models. The new organization makes it more
    explicit that some datasets are spatial while others are not, and
    minimizes code duplication between the two cases.

--- python/activator/middleware_interface.py
+++ python/activator/middleware_interface.py
@@ -592,19 +592,18 @@ class MiddlewareInterface:
         """
         with lsst.utils.timer.time_this(_log, msg="prep_butler (find calibs)", level=logging.DEBUG):
             calib_datasets = set(self._export_calibs(self.visit.detector, self.visit.filters))
-        with lsst.utils.timer.time_this(_log, msg="prep_butler (find ML models)", level=logging.DEBUG):
-            model_datasets = set(self._export_ml_models())
-        if region is None:
-            return (calib_datasets | model_datasets, calib_datasets)
 
-        with lsst.utils.timer.time_this(_log, msg="prep_butler (find refcats)", level=logging.DEBUG):
-            refcat_datasets = set(self._export_refcats(region))
-        with lsst.utils.timer.time_this(_log, msg="prep_butler (find templates)", level=logging.DEBUG):
-            template_datasets = set(self._export_skymap_and_templates(
-                region, self.visit.filters))
-        return (refcat_datasets | template_datasets | calib_datasets | model_datasets,
-                calib_datasets,
-                )
+        all_datasets = calib_datasets
+        with lsst.utils.timer.time_this(_log, msg="prep_butler (find ML models)", level=logging.DEBUG):
+            all_datasets |= set(self._export_ml_models())
+
+        if region is not None:
+            with lsst.utils.timer.time_this(_log, msg="prep_butler (find refcats)", level=logging.DEBUG):
+                all_datasets |= set(self._export_refcats(region))
+            with lsst.utils.timer.time_this(_log, msg="prep_butler (find templates)", level=logging.DEBUG):
+                all_datasets |= set(self._export_skymap_and_templates(
+                    region, self.visit.filters))
+        return (all_datasets, calib_datasets)
 
     def _export_refcats(self, region):
         """Identify the refcats to export from the central butler.
