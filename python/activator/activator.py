7bf88436fe9a9ee5a692544681e632c1894920f4
David H. Irving <david.irving@noirlab.edu>
2025-07-08 15:26:41 -0700

    Add option to use service for Butler outputs
    Added an option to send a Kafka message to a microservice to write output datasets to the central Butler database, instead of connecting directly to the database.  This is intended to reduce database contention, as detailed in DMTN-310.

--- python/activator/activator.py
+++ python/activator/activator.py
@@ -59,6 +59,7 @@ from .exception import GracefulShutdownInterrupt, IgnorableVisit, InvalidVisitEr
     NonRetriableError, RetriableError
 from .middleware_interface import get_central_butler, \
     make_local_repo, make_local_cache, MiddlewareInterface, ButlerWriter, DirectButlerWriter
+from .kafka_butler_writer import KafkaButlerWriter
 from .repo_tracker import LocalRepoTracker
 
 # Platform that prompt processing will run on
@@ -95,6 +96,23 @@ bucket_notification_kafka_offset_reset = os.environ.get("BUCKET_NOTIFICATION_KAF
 max_requests = int(os.environ.get("WORKER_RESTART_FREQ", 0))
 # The number of seconds to delay retrying connections to the Redis stream.
 redis_retry = float(os.environ.get("REDIS_RETRY_DELAY", 30))
+
+# If '1', sends outputs to a service for transfer into the central Butler
+# repository instead of writing to the database directly.
+use_kafka_butler_writer = os.environ.get("USE_KAFKA_BUTLER_WRITER", "0") == "1"
+if use_kafka_butler_writer:
+    # Hostname of the Kafka cluster used by the Butler writer.
+    butler_writer_kafka_cluster = os.environ["BUTLER_WRITER_KAFKA_CLUSTER"]
+    # Username for authentication to BUTLER_WRITER_KAFKA_CLUSTER.
+    butler_writer_kafka_username = os.environ["BUTLER_WRITER_KAFKA_USERNAME"]
+    # Password for authentication to BUTLER_WRITER_KAFKA_CLUSTER.
+    butler_writer_kafka_password = os.environ["BUTLER_WRITER_KAFKA_PASSWORD"]
+    # Topic used to transfer output datasets to the central repository.
+    butler_writer_kafka_topic = os.environ["BUTLER_WRITER_KAFKA_TOPIC"]
+    # URI to the path where output datasets will be written when using the Kafka
+    # writer to transfer outputs to the central Butler repository.
+    # This will generally be in the same S3 bucket used by the central Butler.
+    butler_writer_file_output_path = os.environ["BUTLER_WRITER_FILE_OUTPUT_PATH"]
 
 # Conditionally load keda environment variables
 if platform == "keda":
@@ -164,6 +182,18 @@ def _get_consumer():
 
 
 @functools.cache
+def _get_producer():
+    """Lazy initialization of Kafka Producer for Butler writer."""
+    return kafka.Producer({
+        "bootstrap.servers": butler_writer_kafka_cluster,
+        "security.protocol": "sasl_plaintext",
+        "sasl.mechanism": "SCRAM-SHA-512",
+        "sasl.username": butler_writer_kafka_username,
+        "sasl.password": butler_writer_kafka_password
+    })
+
+
+@functools.cache
 def _get_storage_client():
     """Lazy initialization of cloud storage reader."""
     storage_client = boto3.client('s3', endpoint_url=s3_endpoint)
@@ -192,7 +222,14 @@ def _get_read_butler():
 @functools.cache
 def _get_butler_writer() -> ButlerWriter:
     """Lazy initialization of Butler writer."""
-    return DirectButlerWriter(_get_write_butler())
+    if use_kafka_butler_writer:
+        return KafkaButlerWriter(
+            _get_producer(),
+            output_topic=butler_writer_kafka_topic,
+            file_output_path=butler_writer_file_output_path
+        )
+    else:
+        return DirectButlerWriter(_get_write_butler())
 
 
 @functools.cache
