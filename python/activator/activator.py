31ebe88d37fe705e0fd38da192fc5706e915180a
Krzysztof Findeisen <kfindeis@uw.edu>
2025-10-07 15:37:07 -0700

    Fix missing index for activator.py.
    Now that activator.py is an importable module, and not an executable,
    it should list the members intended to be imported elsewhere.

--- python/activator/activator.py
+++ python/activator/activator.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__all__ = []
+__all__ = ["time_since", "is_processable", "process_visit"]
 
 import collections.abc
 import contextlib
